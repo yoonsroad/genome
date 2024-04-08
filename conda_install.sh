2cb15453bf5c2787db699cb91b40be053dd70d96
YoonsGit <72344623+yoonsroad@users.noreply.github.com>
2024-04-09 00:27:47 +0900

    Add files via upload

---
+++ conda_install.sh
@@ -0,0 +1,8 @@
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/bwa-mem2.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/fastp.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/flye.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/medaka.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/nextdenovo.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/porechop.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/purge_haplotigs.yaml
+curl -sLO https://raw.githubusercontent.com/yoonsroad/genome/main/tapestry.yaml
